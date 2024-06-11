4f3f802c70305c3c29238af1110e79ada77086a7
Martha waithaka <marthanyangatiwaithaka@gmail.com>
2024-06-11 08:57:02 +0300

    commit

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -19,25 +19,59 @@ spicy_foods = [
 def get_names(spicy_foods):
     return [food["name"] for food in spicy_foods]
 
+names = get_names(spicy_foods)
+print(names)
+
 def get_spiciest_foods(spicy_foods):
     return [food for food in spicy_foods if food["heat_level"] > 5]
 
+spiciest_foods = get_spiciest_foods(spicy_foods)
+print(spiciest_foods)
+
 def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
-        print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+
+print_spicy_foods(spicy_foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for food in spicy_foods:
         if food["cuisine"] == cuisine:
             return food
+    return None  # Return None if no matching cuisine is found
+
+american_food = get_spicy_food_by_cuisine(spicy_foods, "American")
+print(american_food)
 
 def print_spiciest_foods(spicy_foods):
     spiciest_foods = get_spiciest_foods(spicy_foods)
-    print_spicy_foods(spiciest_foods)
+    for food in spiciest_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+
+print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
-    return sum([food["heat_level"] for food in spicy_foods]) / len(spicy_foods)
+    total_heat = sum(food["heat_level"] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    if num_foods == 0:
+        return 0  
+    average = total_heat / num_foods
+    return int(average)
 
-def create_spicy_food(spicy_foods, spicy_food):
-    spicy_foods.append(spicy_food)
+average = get_average_heat_level(spicy_foods)
+print(average)
+
+def create_spicy_food(spicy_foods, new_spicy_food):
+    spicy_foods.append(new_spicy_food)
     return spicy_foods
+
+new_spicy_food = {
+    "name": "Griot",
+    "cuisine": "Haitian",
+    "heat_level": 10,
+}
+
+updated_spicy_foods = create_spicy_food(spicy_foods, new_spicy_food)
+print(updated_spicy_foods)
